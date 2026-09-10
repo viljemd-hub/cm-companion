a3300dfd0572b4cde02d1c9712f2c53f1aae788c
Viljem <viljem.d@gemail.com>
2026-09-10 11:43:36 +0200

    Bump versionName to 0.2.2 for the scroll-fix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = gitCommitCount
-        versionName = "0.2.1"
+        versionName = "0.2.2"
 
         buildConfigField("String", "DEV_BRIDGE_URL", "\"${devSecret("DEV_BRIDGE_URL")}\"")
         buildConfigField("String", "DEV_INSTALLATION_ID", "\"${devSecret("DEV_INSTALLATION_ID")}\"")
